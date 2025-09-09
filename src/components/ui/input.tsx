5ae2b9b7f8f25b53993f88911355faa380676b7f
Paul-Blankson <paul.blankson@amalitech.com>
2025-09-09 09:20:39 +0000

    refactor: Update Input component to use type alias for props and enhance styling with additional text color and placeholder styles

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,20 +1,22 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => (
-  <input
-    ref={ref}
-    className={cn(
-      "h-9 px-3 rounded-md border border-gray-300 dark:border-white/10 bg-white dark:bg-white/5 text-sm",
-      "focus:outline-none focus:ring-2 focus:ring-purple-500/40",
-      className
-    )}
-    {...props}
-  />
-));
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => (
+    <input
+      ref={ref}
+      className={cn(
+        "h-9 px-3 rounded-md border border-gray-300 dark:border-white/10 bg-white dark:bg-white/5 text-sm",
+        "text-black placeholder:text-gray-400 dark:text-white dark:placeholder:text-white/50",
+        "focus:outline-none focus:ring-2 focus:ring-purple-500/40",
+        className
+      )}
+      {...props}
+    />
+  )
+);
 Input.displayName = "Input";
 
 export { Input };
-
